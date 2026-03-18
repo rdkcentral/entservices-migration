003cdd9a09fecca4ea96c437398b347bde6dd32a
MukeshkumarK <94189462+MukeshkumarK@users.noreply.github.com>
2026-03-18 18:44:34 +0530

    Update test_Migration.cpp

--- Tests/L1Tests/tests/test_Migration.cpp
+++ Tests/L1Tests/tests/test_Migration.cpp
@@ -30,7 +30,7 @@
 #include "ServiceMock.h"
 #include "COMLinkMock.h"
 #include "RfcApiMock.h"
-#include "Telemetry.h"
+#include "TelemetryMock.h"
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
 using ::testing::NiceMock;
